09ab7242970edc425e633aec19d2b12da1402a56
jacobcalv <jacobcalvino@outlook.com>
2019-11-03 18:43:28 -0600

    .

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,30 @@
-// Test away
+// Test away// Test away
+
+import React from 'react';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"
+
+import Dashboard from './Dashboard'
+
+  // defaults to unlocked and open
+test('Dashboard renders with the Display and Controls', () => {
+    const wrapper = rtl.render(<Dashboard />);
+  
+    const unLocked = wrapper.getByText(/unlocked/i);
+    expect(unLocked).toBeInTheDocument();
+  
+    const open = wrapper.getByText(/open/i);
+    expect(open).toBeInTheDocument();
+})
+
+    //controls / display
+test('Dashboard renders and matches snapshot', () => {
+    const wrapper = rtl.render(<Dashboard />);
+  
+    expect(wrapper.asFragment()).toMatchSnapshot();
+})
+  
+// test("<Dashboard /> snapshot", async () => {
+//     const wrapper = rtl.render(<Dashboard />)
+//     expect(wrapper.asFragment()).toMatchSnapshot()
+//   })
