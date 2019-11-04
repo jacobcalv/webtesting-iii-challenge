7e7239e85126c40ee56d89c7c005d8d80b0b2b49
jacobcalv <jacobcalvino@outlook.com>
2019-11-03 19:57:40 -0600

    added snapshots for each component and have 7tests total

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from 'react';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"
+
+import Controls from './Controls'
+
+//match to the snapshot
+
+test('match snapshot for controls', () => {
+    const wrapper = rtl.render(<Controls/>)
+    expect(wrapper.asFragment()).toMatchSnapshot()
+})
+
+test('button render to screen', () => {
+    const wrapper = rtl.render(<Controls/>)
+    const buttons = wrapper.getAllByRole('button')
+
+    expect(buttons).toBeVisible()
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,34 @@
 // Test away!
+import React from 'react';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"
+
+import Display from './Display'
+
+//Display Snapshot and ensures that file displays
+
+test('should display and match snapshot', () => {
+    const wrapper = rtl.render(<Display/>)
+    expect(wrapper.asFragment()).toMatchSnapshot()
+    wrapper.debug()
+})
+
+test('should display unlocked at start', () => {
+    const wrapper = rtl.render(<Display/>)
+    const unlocked = wrapper.queryByText(/unlocked/i)
+    expect(unlocked).toBeVisible();
+})
+
+// test('Door Closes', async() => {
+//     const wrapper = rtl.render(<Dashboard/>)
+//     await wrapper.findByText(/open/i)
+    
+//     const close = wrapper.getByText(/close/i)
+    
+//     rtl.act(() => {
+//         rtl.fireEvent.click(close)
+//     })
+//     expect(wrapper.getByText(/close/i)).not.toBe(/open/i)
+// })
+
+test('')
